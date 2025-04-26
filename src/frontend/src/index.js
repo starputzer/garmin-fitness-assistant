a012852575e8a808684172a04d73628e7a41cfec
starputzer <martin@danglefeet.com>
2025-04-26 19:46:37 +0200

    react create

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import '../styles/main.css';
+import App from '../App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
